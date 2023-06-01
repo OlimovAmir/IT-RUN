c4f29f28e6a7d66e3d4c788d9faa34c55b1481b3
olimov Amir <olimov.amir@mail.ru>
2023-06-01 17:14:16 +0300

    day1

---
+++ D3/ManagedAndUnmanaged/FileManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedAndUnmanaged
+{
+    internal class FileManager : IDisposable
+    {
+        StreamWriter StreamWriter;
+        public FileManager()
+        {
+            StreamWriter = new StreamWriter("store.txt", true);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        bool disposed;
+        protected void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                //Clean up managed code
+                StreamWriter.Dispose();
+            }
+            //clean up unmanaged code
+            disposed = true;
+        }
+
+        ~FileManager()
+        {
+            Dispose(false);
+        }
+
+        public void WriteProduct(string name, int count)
+        {
+            StreamWriter.WriteLine($"{name} {count}");
+            StreamWriter.Flush();
+        }
+    }
+}
---
+++ D3/ManagedAndUnmanaged/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ManagedAndUnmanaged;
+using static System.Net.Mime.MediaTypeNames;
+
+while (true)
+{
+    Console.WriteLine("Choose operation:");
+
+    var operation = Console.ReadLine();
+    if (operation == "enter")
+    {
+        Console.WriteLine("Name:");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Count:");
+        var count = int.Parse(Console.ReadLine());
+
+        using (FileManager application = new FileManager())
+        {
+            application.WriteProduct(name, count);
+        }
+    }
+}
